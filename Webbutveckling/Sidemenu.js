591d82ff781f75484d9c4f4b4a678c7cd1bbfaa6
lucke111222 <lucas.liss@skola.taby.se>
2018-09-03 11:59:45 +0200

    Sker

---
+++ Webbutveckling/Sidemenu.js
@@ -0,0 +1,8 @@
+ function openNav() { /* inställningar för att öppna sid menyn*/
+    document.getElementById("mySidenav").style.width = "250px";
+ }
+ 
+ function closeNav() { /* inställningar för att stänga sid menyn */
+    document.getElementById("mySidenav").style.width = "0";
+ }
+ 
