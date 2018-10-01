c047ba1d999ba4779b294af4efd8472584e16945
Max <max.wetterstrom@skola.taby.se>
2018-10-01 10:45:41 +0200

    Namn och strukturbyte

--- RCOS/Sidemenu.js
+++
@@ -1,7 +0,0 @@
-function openNav() { /* inställningar för att öppna sid menyn*/
-    document.getElementById("mySidenav").style.width = "250px";
- }
- 
- function closeNav() { /* inställningar för att stänga sid menyn */
-    document.getElementById("mySidenav").style.width = "0";
- }
