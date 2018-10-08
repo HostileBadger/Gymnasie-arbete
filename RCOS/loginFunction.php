c7fbddaa771961f7d25c4cc237017e718dbb8b17
MCvoidcraft <42734504+MCvoidcraft@users.noreply.github.com>
2018-10-08 11:54:09 +0200

    inloggnings funktion

---
+++ RCOS/loginFunction.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+    $user_data = file_get_contents("users.txt");
+    $user_array = explode(" ", $user_data);
+    $uid = $_POST["uid"];
+    $_SESSION["user"] = $uid;
+    $pwd =  $_POST["pwd"];
+    foreach ($user_array as $key => $value)
+    {
+        $user_test = explode(":", $user_array[$key]);
+
+        if($uid == $user_test[0] && $pwd == $user_test[1])
+        {
+            header("Location: loggedIn.php");
+            exit();
+        }
+    }
+
+    echo "Access Denied";
+    exit();
+ ?>
