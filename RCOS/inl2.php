6cc76a5c4a47fa8716f27483e815cd18ea194a11
congo6 <cheehoe.oh@skola.taby.se>
2018-10-08 10:24:44 +0200

    'hej'

---
+++ RCOS/inl2.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="welcome.php" method="post">
+    <input type="text" name="name" placeholder="användarnamn"></input>
+    <input type="password" name="password" placeholder="lösenord"></input>
+    <input type="submit" name="submit">
+    </form>
+    <p>användarnamn: qwehej123</p>
+    <p>lösenord: 123</p>
+</body>
+
+<!--
+inlämningsuppgift formulärhantering med PHP
+
+Gör en enkel inloggningssida i PHP! 
+
+Gör en webbsida med ett formulär där användaren får fylla i (1) användarnamn och (2) lösenord. 
+När man sedan trycker på skicka-knappen så vidarebefordras man till en annan webbsida. På denna webbsida ska det antingen stå ett välkomstmeddelande (ifall användarnamn och lösenord matchar) eller ett felmeddelande (ifall inloggningen misslyckas). 
+
+Kolla på lärarens och w3schools exempel om PHP-formulär. Använd någon lämplig if-sats för att kolla att användarnamn och lösenord matchar ett par fördefinierade värden (som kan skrivas direkt i din PHP-kod eller läsas in från en textfil). 
+
+Frivillig utvidgning: Gör så att flera olika användare kan logga in och skriva meddelanden till varandra.
+
+Lämna in i Google Classroom!
+
+ -->
+</html>
+
