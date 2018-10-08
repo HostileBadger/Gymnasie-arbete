407a579498df35a539cdb2f82545323d0c020d23
MCvoidcraft <42734504+MCvoidcraft@users.noreply.github.com>
2018-10-08 15:20:58 +0200

    hej

--- RCOS/loginFunction.php
+++ RCOS/loginFunction.php
@@ -11,7 +11,7 @@ session_start();
 
         if($uid == $user_test[0] && $pwd == $user_test[1])
         {
-            header("Location: profilepage.html");
+            header("Location: profilepage.php");
             exit();
         }
     }
