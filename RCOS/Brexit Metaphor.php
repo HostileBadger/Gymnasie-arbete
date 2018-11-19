4607924daeda73a25fbfe6539773aa05b8683776
lucke111222 <lucas.liss@skola.taby.se>
2018-11-19 13:06:40 +0100

    lol

---
+++ RCOS/Brexit Metaphor.php
@@ -0,0 +1,15 @@
+<?php include "header.php"?>
+
+<?php
+    include "navBar.php";
+  ?>
+
+
+
+<div class="Movies">
+<a target="_blank" href="Brexit Metaphor.php"><img alt="gastrogate.png" src="pics/Brexit_Metaphor.jpg"></a>
+ </div>
+
+
+
+  <?php include "footer.php"?> 
