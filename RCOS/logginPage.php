869be4138b966459cd1faee873bd384c8e708683
lucke111222 <lucas.liss@skola.taby.se>
2018-11-19 11:37:08 +0100

    hej

--- RCOS/logginPage.php
+++ RCOS/logginPage.php
@@ -2,15 +2,8 @@
 <html>
     <?php include "header.php"?>
     <body>
-<<<<<<< HEAD
     <?php include "navBar.php"?>
     <div id="signupform">
-=======
-        <?php include "navBar.php"?>
-        <form method="post" action="index.php">
-            <button type="submit" name="submit">Home</button>
-        </form>
->>>>>>> 714c4064ecc44bb8aaf3c2e63eb9a8a0a5f41e13
         <h1>Sign in to existing account</h1>
         <form method="post" action="loginFunction.php">
             <input type="text" name="uid" placeholder="Username">
