7527bcdcdec7fcbb4b20f18d9bf9515c96ea7a9c
Max <max.wetterstrom@skola.taby.se>
2018-11-19 11:11:09 +0100

    69 420

--- RCOS/accountCreationPage.php
+++ RCOS/accountCreationPage.php
@@ -1,14 +1,9 @@
 <!DOCTYPE html>
 <html>
+    <?php include "header.php"?>
     <body>
-<<<<<<< HEAD
     <?php include "navBar.php" ?>
     <div id="signupform">
-=======
-        <form method="post" action="index.php">
-            <button type="submit" name="submit">Home</button>
-        </form>
->>>>>>> 714c4064ecc44bb8aaf3c2e63eb9a8a0a5f41e13
         <h1>Create new account</h1>
         <form method="post" action="accountCreationFunction.php">
             <input type="text" name="user_uid" placeholder="Username" required>
@@ -24,21 +19,13 @@
             <input type="date" name="user_dob" placeholder="DoB" required>
             <br>
             <button type="submit" name="submit">Create account</button>
-        </form>
-<<<<<<< HEAD
-    
-=======
-
->>>>>>> 714c4064ecc44bb8aaf3c2e63eb9a8a0a5f41e13
+        </form>    
         <br>
         
         <form method="post" action="logginPage.php">
             <button type="submit" name="submit">Sign in to existing account</button>
         </form>
-<<<<<<< HEAD
         </div>
         <?php include "footer.php" ?>
-=======
->>>>>>> 714c4064ecc44bb8aaf3c2e63eb9a8a0a5f41e13
     </body>
 </html>
--- RCOS/logginPage.php
+++ RCOS/logginPage.php
@@ -2,24 +2,15 @@
 <html>
     <?php include "header.php"?>
     <body>
-<<<<<<< HEAD
     <?php include "navBar.php"?>
     <div id="signupform">
-=======
-        <?php include "navBar.php"?>
-        <form method="post" action="index.php">
-            <button type="submit" name="submit">Home</button>
-        </form>
->>>>>>> 714c4064ecc44bb8aaf3c2e63eb9a8a0a5f41e13
         <h1>Sign in to existing account</h1>
         <form method="post" action="loginFunction.php">
             <input type="text" name="uid" placeholder="Username">
             <input type="password" name="pwd" placeholder="Password">
             <button type="submit" name="submit">Login</button>
         </form>
-
         <br>
-
         <form method="post" action="accountCreationPage.php">
             <button type="submit" name="submit">Create new account</button>
         </form>
